b70d5e639e5a5130062473c8b0d31036d33e3723
Emiliano Politi <emipoliti.96@gmail.com>
2022-10-18 22:39:22 -0300

    Aplicando BP a galeria

--- src/pages/GaleriaPage.js
+++ src/pages/GaleriaPage.js
@@ -1,58 +1,388 @@
 import React from "react";
+import "../styles/components/pages/GaleriaPages.css";
 
 const GaleriaPage = (props) => {
   return (
     <main className="holder">
-      <h2>Autos 0km</h2>
+      <h2>Autos 0km:</h2>
 
       <div className="autos">
-        <img
-          src="/public/Img/0km/7CUDN3AWGBBWFAXL6PTQVCKWWI.webp"
-          alt="Auto1"
-        />
-        <img src="/public/Img/0km/CMGFH5HCWNGB7PPPQSTUYT7UMQ.jpg" alt="Auto2" />
-        <img
-          src="/public/Img/0km/Fiat-Argo-lanzamiento-Argentina.jpg"
-          alt="Auto3"
-        />
-        <img src="/public/Img/0km/PELU62OTU5BT5IZMIIYRFMTKDQ.jpg" alt="Auto4" />
-        <img src="/public/Img/0km/ZTQTCKF2DJGCNLJTF6FZS2URUU.jpg" alt="Auto5" />
+        <div>
+          <div className="card" style={{ width: "18rem" }}>
+            <img
+              src="./Img/0km/Gol-Trend.webp"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">Gol Trend:</h5>
+              <p className="card-text">
+                <ul>
+                  <li>0km</li>
+                  <li>1.6</li>
+                  <li>Nafta</li>
+
+                </ul>
+
+
+              </p>
+              <a href={"/"} className="btn btn-primary">
+                COMPRAR
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="card" style={{ width: "18rem" }}>
+            <img
+              src="./Img/0km/Chevrolet.jpg"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">Chevrolet Agile:</h5>
+              <p className="card-text">
+                <ul>
+                  <li>0 km</li>
+                  <li>1.6</li>
+                  <li>Nafta</li>
+
+                </ul>
+              </p>
+              <a href={"/"} className="btn btn-primary">
+                COMPRAR
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="card" style={{ width: "18rem" }}>
+            <img
+              src="./Img/0km/Fiat-Argo.jpg"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">Fiat Argo:</h5>
+              <p className="card-text">
+                <ul>
+                  <li>0 km</li>
+                  <li>1.4</li>
+                  <li>Nafta</li>
+
+                </ul>
+              </p>
+              <a href={"/"} className="btn btn-primary">
+                COMPRAR
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="card" style={{ width: "18rem" }}>
+            <img
+              src="./Img/0km/Fiat.jpg"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">Fiat Uno:</h5>
+              <p className="card-text">
+                <ul>
+                  <li>0 km</li>
+                  <li>1.0</li>
+                  <li>Nafta</li>
+
+                </ul>
+              </p>
+              <a href={"/"} className="btn btn-primary">
+                COMPRAR
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="card" style={{ width: "18rem" }}>
+            <img
+              src="./Img/0km/Fiat-cronos.jpg"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">Fiat Cronos:</h5>
+              <p className="card-text">
+                <ul>
+                  <li>0 km</li>
+                  <li>1.8</li>
+                  <li>Nafta</li>
+
+                </ul>
+              </p>
+              <a href={"/"} className="btn btn-primary">
+                COMPRAR
+              </a>
+            </div>
+          </div>
+        </div>
+
+
       </div>
 
       <h2>Usados:</h2>
 
       <div className="usados">
-        <img
-          src="/public/Img/usados/143744-whatsapp-20image-202021-02-25-20at-2011-12-05.webp"
-          alt="Usados1"
-        />
-        <img
-          src="/public/Img/usados/20201127185430_vw-gol-2014.jpg"
-          alt="Usados2"
-        />
-        <img
-          src="/public/Img/usados/4JSCM4RG65FODK3SWS3C766A7E.jpg"
-          alt="Usados3"
-        />
-        <img
-          src="/public/Img/usados/RT_PU_c02c0f2f94b845a683ca8722b9d26aba.jpg"
-          alt="Usados4"
-        />
-        <img src="/public/Img/usados/vw-gol-cca-usados.webp" alt="Usados5" />
+      <div>
+          <div className="card" style={{ width: "18rem" }}>
+            <img
+              src="./Img/usados/Astra.webp"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">Chevrolet Astra:</h5>
+              <p className="card-text">
+                <ul>
+                  <li>Modelo 2015</li>
+                  <li>60.000 km</li>
+                  <li>Nafta</li>
+
+                </ul>
+
+
+              </p>
+              <a href={"/"} className="btn btn-primary">
+                COMPRAR
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="card" style={{ width: "18rem" }}>
+            <img
+              src="./Img/usados/Corolla.jpg"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">Toyota Corolla:</h5>
+              <p className="card-text">
+                <ul>
+                  <li>Modelo 2018</li>
+                  <li>30.000 km</li>
+                  <li>Nafta</li>
+
+                </ul>
+              </p>
+              <a href={"/"} className="btn btn-primary">
+                COMPRAR
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="card" style={{ width: "18rem" }}>
+            <img
+              src="./Img/usados/Gol-Power.jpg"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">VW Gol:</h5>
+              <p className="card-text">
+                <ul>
+                  <li>Modelo 2012</li>
+                  <li>110.000 km</li>
+                  <li>Nafta</li>
+
+                </ul>
+              </p>
+              <a href={"/"} className="btn btn-primary">
+                COMPRAR
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="card" style={{ width: "18rem" }}>
+            <img
+              src="./Img/usados/Polo.jpg"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">VW Polo:</h5>
+              <p className="card-text">
+                <ul>
+                  <li>Modelo 2021</li>
+                  <li>10.000 km</li>
+                  <li>Nafta</li>
+
+                </ul>
+              </p>
+              <a href={"/"} className="btn btn-primary">
+                COMPRAR
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="card" style={{ width: "18rem" }}>
+            <img
+              src="./Img/usados/Gol-Trend.webp"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">VW Gol Trend:</h5>
+              <p className="card-text">
+                <ul>
+                  <li>Modelo 2014</li>
+                  <li>90.000 km</li>
+                  <li>Nafta</li>
+
+                </ul>
+              </p>
+              <a href={"/"} className="btn btn-primary">
+                COMPRAR
+              </a>
+            </div>
+          </div>
+        </div>
+
       </div>
       <h2>Motos:</h2>
       <div className="motos">
-        <img
-          src="/public/Img/motos/honda-125-cb-twister-uno___Vzp1TLfvv_2000x1500__1.webp"
-          alt="Motos1"
-        />
-        <img
-          src="/public/Img/motos/Honda_CBR500R_2022-1024x576.jpg"
-          alt="Motos2"
-        />
-        <img src="/public/Img/motos/maxresdefault.jpg" alt="Motos3" />
-        <img src="/public/Img/motos/motors-22.jpg" alt="Motos4" />
-        <img src="/public/Img/motos/rouser-ns-200-fi.webp" alt="Motos5" />
+      <div>
+          <div className="card" style={{ width: "18rem" }}>
+            <img
+              src="./Img/motos/Honda_CBR.jpg"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">Honda CBR:</h5>
+              <p className="card-text">
+                <ul>
+                  <li>Modelo 2020</li>
+                  <li>10.000 km</li>
+                  <li>800 CC</li>
+
+                </ul>
+
+
+              </p>
+              <a href={"/"} className="btn btn-primary">
+                COMPRAR
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="card" style={{ width: "18rem" }}>
+            <img
+              src="./Img/motos/Gilera-Smash.jpg"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">Gilera Smash:</h5>
+              <p className="card-text">
+                <ul>
+                  <li>Modelo 2018</li>
+                  <li>12.000 km</li>
+                  <li>110 CC</li>
+
+                </ul>
+              </p>
+              <a href={"/"} className="btn btn-primary">
+                COMPRAR
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="card" style={{ width: "18rem" }}>
+            <img
+              src="./Img/motos/rouser.webp"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">Rowser Lince:</h5>
+              <p className="card-text">
+                <ul>
+                  <li>0 km</li>
+                  <li>200 CC</li>
+                  <li>Inyeccion</li>
+
+                </ul>
+              </p>
+              <a href={"/"} className="btn btn-primary">
+                COMPRAR
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="card" style={{ width: "18rem" }}>
+            <img
+              src="./Img/motos/honda-twister.WEBP"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">Honda Twister:</h5>
+              <p className="card-text">
+                <ul>
+                  <li>Modelo 2022</li>
+                  <li>3.000 km</li>
+                  <li>250 CC</li>
+
+                </ul>
+              </p>
+              <a href={"/"} className="btn btn-primary">
+                COMPRAR
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="card" style={{ width: "18rem" }}>
+            <img
+              src="./Img/motos/Rowser-200.jpg"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">Rowser Lince</h5>
+              <p className="card-text">
+                <ul>
+                  <li>Modelo 2020</li>
+                  <li>9.000 km</li>
+                  <li>200 CC</li>
+
+                </ul>
+              </p>
+              <a href={"/"} className="btn btn-primary">
+                COMPRAR
+              </a>
+            </div>
+          </div>
+        </div>
+
+
       </div>
     </main>
   );
