aabdbad4cc8729b626af737ccd453c0540b242cc
Emiliano Politi <emipoliti.96@gmail.com>
2022-10-21 09:17:17 -0300

    Cambios 21/10

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -2,7 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "../../styles/components/layout/Nav.css";
 
-const Nav = (props) => {
+const Nav = (props) => { 
   return (
     <nav>
       <div>
